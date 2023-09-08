25697438d741cf94b563821387951224786938bb
yinzhiqing <yinzhiqing@163.com>
2023-09-08 17:58:15 +0800

    test proof parameter

---
+++ scripts/mint_proof_parameter.js
@@ -0,0 +1,106 @@
+const fs        = require('fs');
+const path      = require("path");
+const program   = require('commander');
+const utils     = require("./utils");
+const logger    = require("./logger");
+const prj       = require("../prj.config.js");
+
+const bak_path  = prj.caches_contracts;
+const tokens  = require(prj.contract_conf);
+const {ethers, upgrades}    = require("hardhat");
+
+async function get_contract(name, address) {
+    return await utils.get_contract(name, address);
+}
+
+async function show_accounts() {
+    const accounts = await ethers.provider.listAccounts();
+    console.log(accounts);
+}
+
+async function has_role(cobj, address, role) {
+    let brole = web3.eth.abi.encodeParameter("bytes32", web3.utils.soliditySha3(role));
+    let has = await cobj.hasRole(brole, address);
+
+    return has;
+}
+
+async function count_of(client) {
+    let count = await client.totalSupply();
+    logger.debug(count);
+    return count;
+}
+
+async function create_token_id(data) {
+    return web3.utils.sha3(data.toString());
+}
+
+async function contract(name) {
+    let token = tokens[name];
+    return await get_contract(token.name, token.address);
+}
+async function run() {
+    logger.debug("start working...", "mint");
+
+    let proof_parameter = await contract("SCPNSProofParameter");
+
+    let role   = "MINTER_ROLE";
+    let signer = ethers.provider.getSigner(0); 
+    let minter = await signer.getAddress(); 
+
+    let has_miter = await has_role(proof_parameter, minter, role);
+    if (has_miter != true) {
+        logger.error(personal + " no minter role." );
+        return;
+    } 
+
+
+    let rows = [];
+    let token_ids = [];
+
+    let tokens = {};
+    tokens["min"] = {leaf_count: 100000000,       leaf_deep: 1000000};
+    tokens["mid"] = {leaf_count: 10000000000,     leaf_deep: 100000000};
+    tokens["max"] = {leaf_count: 10000000000000,  leaf_deep: 10000000000};
+
+    for (var key in tokens) {
+        let token = tokens[key];
+        let token_id = await create_token_id(key);
+        let token_name = key;
+
+        token_ids.push(token_id);
+
+        let isExists = await proof_parameter.exists(token_id); 
+        if (isExists == true) {
+            logger.debug("token(id = " + token_id +" name =" + token_name + ") is exists");
+            continue;
+        }
+
+        let pnames = [];
+        let pvalues = [];
+        for (var name in token) {
+            pnames.push(utils.str_to_w3bytes32(name));
+            pvalues.push(token[name]);
+        }
+
+        token_name = utils.str_to_w3bytes32(token_name);
+        let datas = utils.json_to_w3str({data: key});
+        logger.debug("new token: " + token_id);
+  
+        let tx = await proof_parameter.connect(signer).mint(token_id, token_name, pnames, pvalues, datas);
+
+        rows.push({
+            token_id: token_id,
+            token_name: key
+        });
+    }
+    logger.table(rows, "new tokens");
+
+}
+
+run()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1)
+  });
---
+++ scripts/show_proof_parameters.js
@@ -0,0 +1,79 @@
+
+const fs        = require('fs');
+const path      = require("path");
+const program   = require('commander');
+const utils     = require("./utils");
+const logger    = require("./logger");
+const prj       = require("../prj.config.js");
+
+const bak_path  = prj.caches_contracts;
+const tokens  = require(prj.contract_conf);
+const {ethers, upgrades}    = require("hardhat");
+
+async function get_contract(name, address) {
+    return await utils.get_contract(name, address);
+}
+
+function is_target_name(token_name) {
+    let target_token_name = "SCPNSProofParameter";
+    return (target_token_name == "" || target_token_name == token_name) && token_name != "";
+}
+
+async function show_tokens(token) {
+    let cobj = await get_contract(token.name, token.address);
+    logger.debug("token address: " + token.address);
+
+    let name = await cobj.name();
+    logger.debug("name: " + name);
+
+    let amounts = await cobj.totalSupply();
+    logger.debug("totalSupply: " + amounts);
+    let list = [];
+    for (let i = 0; i < amounts; i++) {
+        let row = new Map();
+        row["tokenId"] = utils.w3uint256_to_hex(await cobj.tokenByIndex(i));
+        row["name"] = utils.w3bytes32_to_str(await cobj.nameOf(row["tokenId"]));
+
+        let pnames = [];
+        let pvalues = [];
+        let pcount = await cobj.parameterCountOf(row["tokenId"]);
+        logger.debug("pcount:" + pcount.toString());
+        for (var pi = 0; pi < pcount; pi++) {
+            let pname = await cobj.parameterNameOf(row['tokenId'], pi);
+            let pvalue = await cobj.valueOfParameter(row["tokenId"], pname);
+            pnames.push(utils.w3bytes32_to_str(pname));
+            pvalues.push(pvalue.toString());
+
+        }
+        logger.debug(pnames);
+        logger.debug(pvalues);
+
+
+        let datas = utils.w3str_to_str(await cobj.datasOf(row["tokenId"]));
+        logger.info("tokenId: " + row["tokenId"], "token info");
+        logger.info("datas: ");
+        logger.info(JSON.parse(datas));
+
+        list.push(row);
+
+    } 
+    logger.table(list);
+}
+
+async function run() {
+    logger.debug("start working...", "show_tokens");
+    for (var token_name in tokens) {
+        if (!is_target_name(token_name)) continue;
+
+        logger.debug("#contract name: " + token_name);
+        token = tokens[token_name];
+        await show_tokens(token);
+    }
+}
+run()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
